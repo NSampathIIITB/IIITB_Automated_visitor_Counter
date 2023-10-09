315420c097cbea6c5d232dabb95e3b2a386d16a9
NSampathIIITB <141038460+NSampathIIITB@users.noreply.github.com>
2023-10-09 18:36:51 +0530

    Add files via upload

---
+++ automated_visitor_counter.c
@@ -0,0 +1,82 @@
+void displaytotalpeople();
+void delay(int);
+int sensor_1 = 0;
+int sensor_2 = 0;
+int total_people = 0;
+
+
+int main()
+{
+    
+    while (1) {
+        // Simulated digitalRead for sensor1  and sensor2(Assuming x30 contains sensor values)
+       
+            asm volatile(
+            "andi %0, x30, 0x01\n\t"
+            "andi %0, x30, 0x02\n\t"
+            : "=r"(sensor_1),"=r"(sensor_2)
+            :
+            );
+
+        if (sensor_1 == 0)
+        {
+            while (sensor_2 == 1)
+            {
+                // Wait for person to fully enter
+                // Replace with your code to check sensor2 state
+                asm volatile(
+            
+            "andi %0, x30, 0x02\n\t"
+            : "=r"(sensor_2)
+            :
+            );
+                
+            }
+
+            // Person entered
+            total_people++;
+            //show();
+            displaytotalpeople();
+            delay(300); // Delay for 300 milliseconds
+        }
+       
+    }
+    return 0;
+}
+
+void displaytotalpeople()
+{
+      int ledpin[5],i,j,ledpin_reg[5];
+      for(i=1; total_people > 0;i++)
+      {
+      ledpin[i] = total_people % 2;
+      total_people = total_people / 2;
+      }
+      ledpin_reg[1] = ledpin[1]*2;
+      ledpin_reg[2] = ledpin[2]*4;
+      ledpin_reg[3] = ledpin[3]*8;
+      ledpin_reg[4] = ledpin[4]*16;
+      ledpin_reg[5] = ledpin[5]*32;
+      
+      asm volatile(
+	"or x30, x30, %0\n\t"
+	"or x30, x30, %1\n\t"
+	"or x30, x30, %2\n\t"
+	"or x30, x30, %3\n\t"
+	"or x30, x30, %4\n\t"
+	: 
+	:"r"(ledpin_reg[1]),"r"(ledpin_reg[2]),"r"(ledpin_reg[3]),"r"(ledpin_reg[4]),"r"(ledpin_reg[5])
+	:"x30"
+	);
+      
+}
+
+
+void delay(int milliseconds) {
+    for (int i = 0; i < milliseconds; i++) {
+        for (volatile int j = 0; j < 10000; j++) {
+            // Adjust the loop count based on your processor's speed
+        }
+    }
+}
+
